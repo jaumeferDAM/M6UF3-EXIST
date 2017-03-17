5b49f70d31f01ecdd9ac5e0f6e8e20bade5f1a5a
Jorge <Jorge@JORGE>
2017-03-17 11:30:02 +0100

    Merge commit 'bfc21b32f82c9464c35ee0fdf88d6282b8fa9b8a'
    Conflicts:
    	Ejemple2/src/ejemple2/Ejemple2.java

--- M06uf3_eXist/src/m06uf3_exist/Put.java
+++ M06uf3_eXist/src/m06uf3_exist/Put.java
@@ -13,44 +13,45 @@ import org.xmldb.api.modules.XMLResource;
 
 /**
  * Add a document to the database.
- * 
+ *
  * Call with java -jar start.jar org.exist.examples.xmldb.Put collection docName
  *
  */
 public class Put {
 
-	public final static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
-	
-	public static void main(String args[]) throws Exception {
+    public final static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
 
-		String collection = "/db/m06uf3", file = "libros.xml";
+    public static void main(String args[]) throws Exception {
+
+        //String collection = "/db/m06uf3", file = "libros.xml";
+        String collection = "/db/m06uf3", file = "comentarios.xml";
 
         // initialize driver
-		String driver = "org.exist.xmldb.DatabaseImpl";
-		Class<?> cl = Class.forName(driver);			
-		Database database = (Database)cl.newInstance();
-		database.setProperty("create-database", "true");
-		DatabaseManager.registerDatabase(database);
-		
+        String driver = "org.exist.xmldb.DatabaseImpl";
+        Class<?> cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
         // try to get collection
-		Collection col = 
-			DatabaseManager.getCollection(URI + collection);
-		if(col == null) {
+        Collection col
+                = DatabaseManager.getCollection(URI + collection);
+        if (col == null) {
             // collection does not exist: get root collection and create.
             // for simplicity, we assume that the new collection is a
             // direct child of the root collection, e.g. /db/test.
             // the example will fail otherwise.
             Collection root = DatabaseManager.getCollection(URI + XmldbURI.ROOT_COLLECTION);
-            CollectionManagementService mgtService = 
-                (CollectionManagementService)root.getService("CollectionManagementService", "1.0");
+            CollectionManagementService mgtService
+                    = (CollectionManagementService) root.getService("CollectionManagementService", "1.0");
             col = mgtService.createCollection(collection.substring((XmldbURI.ROOT_COLLECTION + "/").length()));
         }
-		Path f = Paths.get(file);
+        Path f = Paths.get(file);
         // create new XMLResource
-		XMLResource document = (XMLResource)col.createResource(FileUtils.fileName(f), XMLResource.RESOURCE_TYPE);
-		document.setContent(f);
-		System.out.print("storing document " + document.getId() + "...");
-		col.storeResource(document);
-		System.out.println("ok.");
-	}
+        XMLResource document = (XMLResource) col.createResource(FileUtils.fileName(f), XMLResource.RESOURCE_TYPE);
+        document.setContent(f);
+        System.out.print("Storing document " + document.getId() + "...");
+        col.storeResource(document);
+        System.out.println("ok.");
+    }
 }
